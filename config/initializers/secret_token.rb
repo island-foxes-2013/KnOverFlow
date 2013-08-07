40794b7dec6148ccdf1b78f9964e97a32fe5426b
Maria <mbtomori@gmail.com>
2013-08-07 11:28:53 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KnOverFlow::Application.config.secret_token = '2fd655234b4bd47f18c56a1c92c60697764eac3667a2ba0590782c50a84416f3f9bfad87f00e14abec93722ae085d35fa6fbf6fd063758145c2ac72544f62d10'
