bb93b5ed11e36dcec2e913adefb483a5656dcf58
Nicholas Jayanty <njayanty@Nicholass-MacBook-Pro-2.local>
2013-08-07 18:24:21 -0700

    testing environment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
 
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      session[:user_id] = 
-  end
+  # def create
+  #   @user = User.new(params[:user])
+  #   if @user.save
+  #     session[:user_id] = 
+  #   end
+  # end
+end
