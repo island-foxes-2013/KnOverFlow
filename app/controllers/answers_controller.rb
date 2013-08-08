2b37b92ecbb5b7bf612c0417e1a7b5e29f470a55
Christine <cmarquardt86@gmail.com>
2013-08-08 16:34:20 -0700

    can create answers to the questions and they will display on the question show page

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,3 +1,25 @@
 class AnswersController < ApplicationController
 
+  def index
+  end
+
+  def new
+    @answer = Answer.new
+    @question = Question.find(params[:question_id])
+  end
+
+  def create
+    answer = Answer.new(params[:answer])
+    answer.user_id = current_user.id
+    answer.question_id = params[:question_id]
+    answer.save
+    @answers = Answer.find_by_question_id(params[:question_id])
+    puts @answers
+    redirect_to question_path(answer.question_id)
+  end
+
+  def destroy
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 KnOverFlow::Application.routes.draw do
-  resources :questions
+  resources :questions do
+    resources :answers, only: [:new, :create]
+  end
+  resources :answers, except: [:new, :create]
   root to: 'questions#index'
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
