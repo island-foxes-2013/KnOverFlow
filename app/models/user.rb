f358ea25762583a61661a0fca18b8bba3f25a7e7
Apprentice <apprentice@dbc24.local>
2013-08-07 20:51:36 -0700

    created unit tests on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,11 @@ attr_accessible :name, :email, :password, :password_confirmation
 
   has_secure_password
 
-  validates_presence_of :password, :on => :create
+  # validates_presence_of :password, :on => :create, 
+  validates :password, presence: true, length: { minimum: 6 }
 
+  validates_presence_of :password_confirmation, :on => :create
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,67 @@
+# Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  password   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'spec_helper'
+
+describe User do
+
+  before do
+    @user = User.new(name: "Jared", email: "jared@email.com", password: "good_password", password_confirmation: "good_password")
+  end
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password_confirmation)}
+
+  describe "when email test is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        @user.should_not be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do 
+    it "should be valid" do
+      addresses = %w[user@foo.com A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        @user.should be_valid
+      end
+    end
+  end
+
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+end
