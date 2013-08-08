6b2a5d4f0aadca97b82d49dfe33150d7453fbbcb
Apprentice <apprentice@dbc24.local>
2013-08-07 17:30:52 -0700

    set up new.rb and users controller--no testing

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:error] = "Please try again"
+      render 'new'
+    end
+  end
+
+  def show
+  end
+end
