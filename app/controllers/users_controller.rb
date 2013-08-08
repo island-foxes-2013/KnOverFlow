fdadc11cdffc4b124f752f44ed90cf8f44804ff7
Apprentice <apprentice@dbc24.local>
2013-08-07 17:18:36 -0700

    started users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = 
+  end
