451525a3172636f34e2f43e17c0aa5fb09224e37
Apprentice <apprentice@devbootcamp.com>
2013-08-08 18:37:32 -0700

    i added two files and stuff isnt workign...

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ActionController::Base
+
+  def new
+    @comment = Comment.new
+    @question = Question.find(params[:question_id])
+  end
+
+  # def new
+  #   @answer = Answer.new
+  #   @question = Question.find(params[:question_id])
+  # end
+
+  def create
+  	p params    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 KnOverFlow::Application.routes.draw do
   resources :questions do
+  	resources :comments, only: [:new, :create]
     resources :answers, only: [:new, :create]
   end
-  resources :answers, except: [:new, :create]
+  resources :answers, except: [:new, :create] do
+  	resources :comments, only: [:new, :create]
+  end
   root to: 'questions#index'
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+
 end
