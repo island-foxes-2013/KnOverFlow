315dba29992961dbac53f45ac7f165ff1c9592c3
Nicholas Jayanty <njayanty@Nicholass-MacBook-Pro-2.local>
2013-08-09 12:47:04 -0700

    delete simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,6 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'faker'
-# require 'simplecov'
-# SimpleCov.start
-# ENV["RAILS_ENV"] ||= 'test'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
