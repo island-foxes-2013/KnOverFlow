77df7d3af8c3c2a3ae2ed6baae6475ef95d1b6e5
jaredstander <jaredstander@gmail.com>
2013-08-09 16:52:25 -0700

    Added answer and question spec model files-- cannot test sign in and sign out because of wireframe changes

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,48 @@
+# Schema Information
+#
+# Table name: answers
+#
+# t.text     "content"
+# t.integer  "user_id"
+# t.integer  "question_id"
+# t.datetime "created_at",  :null => false
+# t.datetime "updated_at",  :null => false
+#
+
+require 'spec_helper'
+
+describe Answer do
+
+  let(:user) { FactoryGirl.build(:user) }
+  let(:question) { FactoryGirl.build(:question) }
+  
+  context "#new" do
+    it "should allow mass-assignment on content" do
+      expect {
+        answer = Answer.new(content: "Good on ya!")
+      }.to_not raise_error
+    end
+
+    it "should not allow mass-assignment on user" do
+      expect {
+        answer = Answer.new(user: user)
+      }.to raise_error
+      expect {
+        answer = Answer.new(user_id: user.id)
+      }.to raise_error
+    end
+
+    it "should not allow mass-assignment on question" do
+      expect {
+        answer = Answer.new(question: question)
+      }.to raise_error
+      expect {
+        answer = Answer.new(question_id: question.id)
+      }.to raise_error
+    end
+  end
+
+  context "#save" do
+    #
+  end
+end
---
+++ spec/models/question_spec.rb
@@ -0,0 +1,57 @@
+# Schema Information
+#
+# Table name: questions
+#
+#  id                :integer
+#  title             :string(255)
+#  content           :string(255)
+#  associated user   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'spec_helper'
+
+describe Question do
+ 
+  let(:user) { FactoryGirl.build(:user) }
+  let(:question) { FactoryGirl.build(:question) }
+ 
+  context "#new" do
+    #it "allows mass-assignment on title and content" do
+    it "should accept mass assignments for its title and content fields" do
+      expect {
+        question = Question.new(title: "Thing 1", content: "Things 2")
+      }.not_to raise_error
+    end
+ 
+    it "should not allow mass-assignment on user" do
+      expect {
+        question = Question.new(title: "Thing 1", content: "Thing 2", user: user)
+      }.to raise_error
+      expect {
+        question = Question.new(title: "Thing 1", content: "Thing 2", user_id: user.id)
+      }.to raise_error
+    end
+  end
+ 
+  context "#save" do
+    #it "requires a valid user" do
+    it "should be associated with a user before it can be saved" do
+      expect(question).not_to be_valid
+    end
+ 
+    #it "requires a valid title" do
+    it "should not save when title is blank" do
+      question = FactoryGirl.build(:question, title: "", user: user)
+      expect(question).not_to be_valid
+    end
+ 
+    #it "requires a valid content" do
+    it "should not save when content is blank" do
+      user = FactoryGirl.build(:user)
+      question = FactoryGirl.build(:question, content: "", user: user)
+      expect(question).not_to be_valid
+    end
+  end
+end
