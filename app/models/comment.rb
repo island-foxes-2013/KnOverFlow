8e35162218573e004cb916d557f028af352e353a
jaredstander <jaredstander@gmail.com>
2013-08-09 20:58:57 -0700

    Comment model unit test currently passes 100%

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable, polymorphic: true
 
-  attr_accessible :content, :user_id # The user_id needs to be accessible to use the seed file
+  attr_accessible :content # The user_id needs to be accessible to use the seed file
   # but can be removed after development has ended.
+  # user_id of the comment has been removed from mass assignment and is now done by specific sttribute assignment.
+
+  validates :commentable_id, :content, :user, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,62 +1,79 @@
 # Schema Information
 #
-# Table name: answers
+# Table name: comments
 #
 # t.text     "content"
 # t.integer  "user_id"
-# t.integer  "question_id"
-# t.datetime "created_at",  :null => false
-# t.datetime "updated_at",  :null => false
+# t.integer  "commentable_id"
+# t.string   "commentable_type"
+# t.datetime "created_at",       :null => false
+# t.datetime "updated_at",       :null => false
 #
 
 require 'spec_helper'
 
-describe Answer do
+describe Comment do
 
   let(:user) { FactoryGirl.build(:user) }
   let(:question) { FactoryGirl.build(:question) }
   let(:answer) { FactoryGirl.build(:answer) }
+  let(:comment) { FactoryGirl.build(:comment) }
   
   context "#new" do
     it "should allow mass-assignment on content" do
       expect {
-        answer = Answer.new(content: "Blazzie-blah")
+        comment = Comment.new(content: "Blazzie-blah")
       }.to_not raise_error
     end
 
     it "should not allow mass-assignment on user" do
       expect {
-        answer = Answer.new(user: user)
+        comment = Comment.new(user: user)
       }.to raise_error
       expect {
-        answer = Answer.new(user_id: user.id)
+        comment = Comment.new(user_id: user.id)
       }.to raise_error
     end
 
-    it "should not allow mass-assignment on question" do
+    it "should not allow mass-assignment on commentable_id" do
       expect {
-        answer = Answer.new(question: question)
+        comment = Comment.new(question: question)
       }.to raise_error
       expect {
-        answer = Answer.new(question_id: question.id)
+        comment = Comment.new(question_id: question.id)
+      }.to raise_error
+      expect {
+        comment = Comment.new(answer: answer)
+      }.to raise_error
+      expect {
+        comment = Comment.new(answer_id: answer.id)
+      }.to raise_error
+    end
+
+    it "should not allow mass-assignment on commentable_type" do
+      expect {
+        comment = Comment.new(commentable_type: "any value")
       }.to raise_error
     end
   end
 
   context "#save" do
     it "requires a valid user" do
-      answer = FactoryGirl.build(:answer, content: "DO A BARREL ROLE", question: question)
-      expect(answer).not_to be_valid
+      comment = FactoryGirl.build(:comment, content: "DO A BARREL ROLE", commentable_id: question, commentable_type: "BlastFox")
+      expect(comment).not_to be_valid
     end
 
-    it "requires a valid question" do
-      answer = FactoryGirl.build(:answer, content: "DO A BARREL ROLE", user: user)
-      expect(answer).not_to be_valid
+    # There is no commentable_type test because AR automatically populates the type
+    # when an valid ID is passed to the commentable_id attribute
+    # #ActiveRecordMagic
+    it "requires a valid commentable_id" do
+      comment = FactoryGirl.build(:comment, content: "DO A BARREL ROLE", user: user, commentable_type: "BlastFox")
+      expect(comment).not_to be_valid
     end
 
     it "requires a valid content" do
-      answer = FactoryGirl.build(:answer, content: "", question: question, user: user)
-      expect(answer).not_to be_valid
+      comment = FactoryGirl.build(:comment, content: "", user: user, commentable_id: answer, commentable_type: "BlastFox")
+      expect(comment).not_to be_valid
     end
   end
 end
