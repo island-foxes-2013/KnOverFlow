e5f4a6b1cac1a70b76196d515ca2f677e96a9c48
zmontesd <zmontesd@gmail.com>
2013-08-09 21:22:02 -0700

    fix merge conflict

--- app/assets/javascripts/answers.js
+++ app/assets/javascripts/answers.js
@@ -4,19 +4,31 @@ var AnswersController = {
     // ajax:success (when server returns successfully)
     // ajax:error (when server returns unsuccessfully)
     // ajax:complete (when server returns no matter what)
-    $('form#new_answer').on('ajax:success', this.onSuccess);
+    $(document).on('ajax:beforeSend', 'form#new_answer', this.beforeSend);
+    $(document).on('ajax:success', 'form#new_answer', this.onSuccess);
+    $(document).on('ajax:error', 'form#new_answer', this.onError);
+  },
+
+  beforeSend: function(e) {
+    $(e.target).parent().find('.errors').remove();
   },
 
   onSuccess: function(e, response, status, xhr) {
-    // e is the event
-    // response is a JSON object with an html attribute
-    $('.answers').append(response.html);
-    $(e.target).find('input[name="answer[content]"]').val('');
-    var offset = $('.answers div:last-child').offset();
+    var $answerForm = $(e.target);
+    var $answersDiv = $('.answers')
+    $answersDiv.append(response.html);
+    $answerForm.find('input[name="answer[content]"]').val('');
+    var offset = $answersDiv.find('div:last-child').offset();
     var offsetTop = offset.top;
     $('html, body').animate({
         scrollTop: offsetTop
     }, 500);
+  }, 
+
+  onError: function(e, xhr, status, message) {
+    // use .html and change the arugements
+    $(e.target).parent().html(xhr.responseJSON.html);
+    // $(e.target).after("<div class='error'>"+xhr.responseJSON.messages.join(',')+"</div>");
   }
 };
 
--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -9,19 +9,18 @@ class AnswersController < ApplicationController
   # end
 
   def create
-    @answer = Answer.new(params[:answer])
-    @answer.user_id = current_user.id
-    @answer.question_id = params[:question_id]
-    if @answer.save
-      #@answers = Answer.find_by_question_id(params[:question_id])
-      #render partial: 'answer', locals: { answer: @answer }
+    answer = Answer.new(params[:answer])
+    answer.user_id = current_user.id
+    answer.question_id = params[:question_id]
+    if answer.save
       render json: {
-        html: render_to_string(partial: 'answer', locals: { answer: @answer })
+        html: render_to_string(partial: 'answer', locals: { answer: answer })
       }
-      # redirect_to question_path(@answer.question_id)
     else
-      flash[:error] = "Error!"
-      redirect_to question_path(@answer.question_id)
+      question = Question.find(answer.question_id)
+      render json: {
+        html: render_to_string(partial: 'form', locals: { question: question, answer: answer })
+      }, status: :unprocessable_entity
     end
   end
 
