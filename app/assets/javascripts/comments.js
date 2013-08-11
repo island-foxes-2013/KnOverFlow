c194d3584534273e2201b898ba1f321f66ac3a30
Apprentice <apprentice@dbc21.local>
2013-08-10 17:04:15 -0700

    ajax for questions comments working

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -11,18 +11,26 @@ var CommentsController = {
     // ajax:error (when server returns unsuccessfully)
     // ajax:complete (when server returns no matter what)    
     // listen for something other than document
-    $(document).on('ajax:beforeSend', 'new#new_comment', this.beforeSend);
-    $(document).on('ajax:success', 'new#new_comment', this.onSuccess);
-    $(document).on('ajax:error', 'new#new_comment', this.onError);
+    // $(document).on('ajax:beforeSend', 'form#new_comment', this.beforeSend);
+    $(document).on('ajax:success', 'form#new_comment', this.onSuccess);
+    $(document).on('ajax:error', 'form#new_comment', this.onError);
   },
 
-  beforeSend: function(e) {
-    $(e.target).find('.errors').remove();
-  },
+  // beforeSend: function(e) {
+  //   debugger
+  //   // $(e.target).find('.errors').remove();
+  // },
 
   onSuccess: function(e, response, status, xhr) {
+
     var $commentForm = $(e.target);
-    var $commentsDiv = $('.question_display ul')
-    $commentsDiv.append()
+    var $commentsDiv = $('.comments_display')
+    $commentsDiv.replaceWith(response.html); 
+    $commentForm.find('input[name="question[comment]"]').val('');
+    var offset = $commentsDiv.find('li:last-child').offset();
+  },
+
+  onError: function(e, xhr, status, message) {
+    console.log("failure in javascript!")
   }
 }
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,20 +12,27 @@ class CommentsController < ApplicationController
 
   def create
     if params.has_key?('question_id')
-      comment = Question.find(params[:question_id]).comments.new(params[:comment])
+      question = Question.find(params[:question_id])
+      comment = question.comments.new(params[:comment])
       comment.user_id = current_user.id
       if comment.save
-        puts "Success!"
+        render json: {
+          html: render_to_string(partial: 'comments', locals: {commentable: question})
+        }
       else
-        puts "Fail"
+        puts "Failed to save comment for question"
+      end
+    elsif params.has_key?('answer_id')
+      answer = Answer.find(params[:answer_id])
+      answer = answer.comments.new(content: params[:comment][:content])
+      answer.user_id = current_user.id
+      if comment.save
+        render json: {
+          html: render_to_string(partial: 'comments', locals: { commentable: answer})
+        }
+      else
+        puts "Failed to save comment for answer"
       end
     end
-    #   Question.find(params[:question_id]).comments.create(content: params[:comment][:content], user: current_user)
-    #   redirect_to question_path(params[:question_id])
-    # elsif params.has_key?('answer_id')
-    #   answer = Answer.find(params[:answer_id])
-    #   answer.comments.create(content: params[:comment][:content], user_id: session[:id])
-    #   redirect_to question_path(answer.question_id)
-    # end
   end 
 end
