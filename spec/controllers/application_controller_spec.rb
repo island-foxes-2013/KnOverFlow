4ea3bf93cd0233eb0cd3098bcea7227605b6934b
Apprentice <apprentice@dbc21.local>
2013-08-11 21:04:08 -0700

    attempted to create controller tests for private methods

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,15 +3,18 @@ require 'spec_helper'
 describe ApplicationController do
   render_views
 
-  let(:user) { create(:user) }
-  let(:session) { session = user }
 
   describe "current user" do
     pending
+    # user.send(current_user)
   end
 
   describe 'logged in?' do
     pending
+    # user.send(current_user)
+    # @controller = ApplicationController.new
+    # @controller.instance_eval{ current_user }   # invoke the private method
+    # @controller.instance_eval{ @current_user }.should be true
   end
 end
 
