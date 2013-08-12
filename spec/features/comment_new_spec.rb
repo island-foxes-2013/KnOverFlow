1c607f3dfcef7fc2a270665138830ce421a1d13c
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-08-12 16:52:25 -0700

    log in before commenting

--- spec/features/comment_new_spec.rb
+++ spec/features/comment_new_spec.rb
@@ -7,7 +7,7 @@ feature "writing comments", js: true do
   let(:answer) { create(:answer, question: question, user: user) }
 
   before do
-    # log_in(user)
+    log_in(user)
     visit question_path(question)
     click_link "answer question"
   end
@@ -19,11 +19,8 @@ feature "writing comments", js: true do
   # end
 
   scenario "an answer will have a link to create a comment", js: true do
-    pending
-    # sleep(10)
     fill_in "Content", with: "blah"
     click_button "Create Answer"
-    # sleep(10)
     page.should have_content("blah") 
   end
 
